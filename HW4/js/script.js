4712748398a12aa0c1e5c2148304881bc1c6262f
KolosovMax <KolosovMaxA@gmail.com>
2022-04-11 12:41:14 +0300

    Внес сравнение в один цикл

--- HW4/js/script.js
+++ HW4/js/script.js
@@ -10,12 +10,9 @@ console.log(`Новый массив: ${mNumbers}`);
 //Задача 2
 console.log("Задача 2");
 numbers = [];
+let min = 400 , max = 0, minIndex = 0, maxIndex = 0;
 for (let i = 0; i <= 6; i++) {
     numbers[i] = Math.floor(Math.random() * (300 - 5) + 5);
-}
-console.log(numbers);
-let min = numbers[0], max = numbers[0], minIndex = 0, maxIndex = 0;
-for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] >= max) {
         max = numbers[i];
         maxIndex = i;
@@ -25,9 +22,10 @@ for (let i = 0; i < numbers.length; i++) {
         minIndex = i;
     }
 }
+console.log(`Был цикл ${numbers}`);
 numbers[minIndex] = max;
 numbers[maxIndex] = min;
-console.log(numbers);
+console.log(`Стал цикл ${numbers}`);
 //Задача 3
 console.log("Задача 3");
 let arr = [], string;
